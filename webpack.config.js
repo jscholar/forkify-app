79afd849a14e19fbeff561406209a0d39adb38d3
jscholar <jinruncai@gmail.com>
2019-01-19 15:59:04 -0800

    Added webpack-dev-server package and scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './dist')
     }
 }
