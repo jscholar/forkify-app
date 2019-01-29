208175a852926c5493604b6aa0a822fd890b6e9f
jscholar <jinruncai@gmail.com>
2019-01-29 15:02:27 -0800

    Moved search api parameters to config file

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const apiID = '87bd6971'
+export const apiKey = '435b617321f9ccbbbd8272918224caad';
+export const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+export const apiInterface = 'https://api.edamam.com/search';
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
+import {apiID, apiKey, corsProxy, apiInterface} from '../config';
 
 export default class Search {
     constructor(query) {
         this.query = query;
     }
     async querySearch() {
-        const apiID = '87bd6971'
-        const apiKey = '435b617321f9ccbbbd8272918224caad';
-        const apiInterface = 'https://api.edamam.com/search'
-        const corsProxy = 'https://cors-anywhere.herokuapp.com/';
         const numResults = '30'
         try {
             const res = await axios(`${corsProxy}${apiInterface}?app_id=${apiID}&app_key=${apiKey}&q=${this.query}&to=${numResults}`);
